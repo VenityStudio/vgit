b206fa841cf34956b8dfb9568daa51c310e58e16
Maxim Tarasov <yousan4ik@gmail.com>
2019-10-29 21:54:04 +0300

    Initial commit. Spring boot with Angular

---
+++ src/test/java/org/venity/vgit/VGitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.venity.vgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
