c8e965b5d8bc270ca35486d85c9bcced2cf9fd80
Atyom Mishin <themrlokopoff@gmail.com>
2019-10-31 22:20:50 +0300

    Template auth form (Only register)

---
+++ src/main/frontend/src/app/elements/auth-module/auth-module.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-auth-module',
+  templateUrl: './auth-module.component.html',
+  styleUrls: ['./auth-module.component.less']
+})
+export class AuthModuleComponent implements OnInit {
+
+  login = new FormGroup({
+    login: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor(private formGroupBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+  }
+
+}
